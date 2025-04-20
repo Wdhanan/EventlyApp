a56d82c96edab5bc7578dd8529e930981717061b
Hanan Wandji <93015515+Wdhanan@users.noreply.github.com>
2025-04-20 16:10:42 +0200

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,11 +1,8 @@
-import os 
 import sqlite3
 from sqlite3 import Error
 
 def create_connection():
     """Erstelle eine Verbindung zur SQLite-Datenbank."""
-    if not os.path.exists("data"):
-        os.makedirs("data")
     conn = None
     try:
         conn = sqlite3.connect("data/eventmanager.db")  
@@ -80,10 +77,12 @@ def create_tables():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 user_id INTEGER NOT NULL,
                 event_id INTEGER NOT NULL,
+                task_id INTEGER,
                 score INTEGER NOT NULL,
                 timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (user_id) REFERENCES users (id),
                 FOREIGN KEY (event_id) REFERENCES events (id)
+                FOREIGN KEY (task_id) REFERENCES tasks (id)
             )
         """)
             conn.commit()
@@ -91,6 +90,67 @@ def create_tables():
             print(e)
         finally:
             conn.close()
+
+def get_event_by_id(event_id):
+    conn = create_connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM events WHERE id = ?", (event_id,))
+            return cursor.fetchone()
+        except Error as e:
+            print(f"Fehler beim Abrufen des Events: {e}")
+        finally:
+            conn.close()
+    return None
+
+def get_tasks_by_event_id(event_id):
+    conn = create_connection()
+    tasks = []
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM tasks WHERE event_id = ?", (event_id,))
+            rows = cursor.fetchall()
+            for row in rows:
+                tasks.append(row)
+        except Error as e:
+            print(f"Fehler beim Abrufen der Aufgaben: {e}")
+        finally:
+            conn.close()
+    return tasks
+
+def get_task_by_id(task_id):
+    conn = create_connection()
+    task = None
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM tasks WHERE id = ?", (task_id,))
+            task = cursor.fetchone()
+        except Error as e:
+            print(f"Fehler beim Abrufen der Aufgabe: {e}")
+        finally:
+            conn.close()
+    return task
+
+def get_username_by_id(user_id):
+    conn = create_connection()
+    username = None
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT username FROM users WHERE id = ?", (user_id,))
+            result = cursor.fetchone()
+            if result:
+                username = result[0]
+        except Error as e:
+            print(f"Fehler beim Abrufen des Benutzernamens: {e}")
+        finally:
+            conn.close()
+    return username
+
+
 
 if __name__ == "__main__":
     create_tables()
